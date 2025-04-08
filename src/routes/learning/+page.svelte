e54179c6a666b7e7a8fe8698c32b7cd3df5e7f3e
FirsHub <thaksin2982548@gmail.com>
2025-04-08 23:00:16 +0700

    ทดสอบบอทดิสคอร์ด

--- src/routes/learning/+page.svelte
+++ src/routes/learning/+page.svelte
@@ -1,155 +1,44 @@
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello worldHello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello worldHello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
+<script>
+    import "../../app.css"
+</script>
 
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
-Hello world
+<div class="box_category_type items-center px-5">
+    <a class="type bgfdc500"href="/">Math</a>
+    <a class="type bgfdc500"href="/">Physics</a>
+    <a class="type bgfdc500"href="/">English</a>
+</div>
+
+<div class="Container_game">
+    <div class="game bg00509d"></div>
+    <div class="game bg00509d"></div>
+    <div class="game bg00509d"></div>
+</div>
+
+
+<style>
+.box_category_type{
+    display: flex;
+    height: 52px;
+    text-align: center;
+}
+
+.type{
+    margin-left: 10px;
+    width: 100px;
+    height: 28px;
+    border-radius: 7px;
+}
+
+.game{
+    width: 100%;
+    height: 240px;
+    border-radius: 7px;
+    margin: 5px;
+}
+
+.Container_game{
+    display: flex;
+    width: 60%;
+    margin-left: 20%;
+}
+</style>
