660faad4fa671e592a1a56976f77bf2769e0af58
guyssar-db <robloxhack1334@gmail.com>
2025-04-09 14:08:57 +0700

    Update Easy

--- src/routes/learning/easy/+page.svelte
+++ src/routes/learning/easy/+page.svelte
@@ -1,19 +1,26 @@
 <script>
     let name = 'name';
     let description = 'description';
-    let tags = 'tags';
+    let tags = 'Thai';
     let index = '0';
+    let ll = "{";
+    let rr = "},"
 </script>
 
 <div class="flex w-full h-[100px] px-4 py-3 items-center text-center justify-start gap-5">
         <input class="border" type="text" name="" id=""  bind:value={name} >
         <input class="border" type="text" name="" id=""  bind:value={description} >
-        <input class="border" type="text" name="" id=""  bind:value={tags} >
+        <select class="border" name="" id="" bind:value={tags}>
+            <option value="Thai">Thai</option>
+            <option value="Physics">Physics</option>
+            <option value="Math">Math</option>
+            <option value="English">English</option>
+        </select>
         <input class="border" type="text" name="" id=""  bind:value={index} >
 </div>
 
 
 <!-- Output -->
 <div class="p-5 bg-sky-50">
-    <p class="">title: '{name}' , description: '{description}', tags: ['{tags}'], index:['{index}'] </p>
+    <p>{ll} title: '{name}' , description: '{description}', tags: ['{tags}'], index:['{index}'] {rr}</p>
 </div>
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-import phaser from '@phaser';
 export default defineConfig({
   plugins: [
     tailwindcss(),
     sveltekit(),
-    phaser(),
   ],
 });
